cea0b059fa6fdfdfa3864c0970928dbbabc6513d
gauravc574 <gauravchawre580@gmail.com>
2022-07-08 19:39:51 +0530

    Day 1

---
+++ MCVControllerDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MCVControllerDemo.MvcApplication" Language="C#" %>
---
+++ MVCDemo/Notes/Notes.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCDemo.Notes
+{
+
+    /* Flow of MVC (Model - View - Controller ) */
+
+    // User hits a URL -> request goes to controller -> control goes to model and collect data -> and then it passes the data to view -> view is dispayed to the user back
+
+    // MVC is a design pattern
+    //With ASP.Net MVC we can make webApps
+    public class Notes
+    {
+    }
+}
