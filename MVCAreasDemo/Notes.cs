7fdab983d754587c00f7c70d94f479af25ccc2b2
gauravc574 <gauravchawre580@gmail.com>
2022-07-12 15:01:53 +0530

    Basic Areas hands - on done

---
+++ MVCAreasDemo/Notes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAreasDemo
+{
+
+    /*A area is a small unit of MVC web application */
+    //same folder structure as actual mvc application
+
+    //isse large asp.net app ko hum smaller functional group me partition kr skte
+
+
+    //areas is a feature to organize related functionality into a
+    //group as a separate namespace (for routing) and folder structure (for view)
+    public class Notes
+    {
+    }
+}
