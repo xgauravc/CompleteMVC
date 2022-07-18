0e2badd04ecd1148c72ac778a4ca42d42250df49
gauravc574 <gauravchawre580@gmail.com>
2022-07-18 13:55:02 +0530

    Started html helpers

---
+++ MVCHtmlHempersDemo/Notes.cs
@@ -0,0 +1,18 @@
+﻿namespace MVCHtmlHempersDemo
+{
+    //Html helpers are c# methods use to return html string
+
+    //HTML Types ->
+    /*
+     * 1. Inline HTML helpers
+     * 2. Built in HTML helpers -
+     *                      a. Standard HTML Helpers
+     *                      b. Strongly typed HTML helpers
+     *                      c. Templated HTML helpers
+     * 3. Custom HTML helpers
+     
+     */
+    public class Notes
+    {
+    }
+}
