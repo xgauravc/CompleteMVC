3550237ff76b28ed8af086f8dcdf2b529bc020ab
gauravc574 <gauravchawre580@gmail.com>
2022-07-19 22:33:37 +0530

    Strongly typed completely

---
+++ MVCHtmlHempersDemo/Controllers/StronglyTypedDemoController.cs
@@ -0,0 +1,31 @@
+﻿using MVCHtmlHempersDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHtmlHempersDemo.Controllers
+{
+    public class StronglyTypedDemoController : Controller
+    {
+        List<Employee> employees = new List<Employee>();
+        // GET: StronglyTypedDemo
+
+        //Model(property)+ View(HTML) = strongly typed view
+        public ActionResult Index()
+        {
+            Employee emp = new Employee() { EmpId = 1, EmpName = "Gaurav", Department = "Engg", IsEmployee = true };
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee emp)
+        {
+            employees.Add(emp);
+
+
+            return View();
+        }
+    }
+}
---
+++ MVCHtmlHempersDemo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHtmlHempersDemo.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Department { get; set; }
+        public bool IsEmployee { get; set; }
+    }
+}
