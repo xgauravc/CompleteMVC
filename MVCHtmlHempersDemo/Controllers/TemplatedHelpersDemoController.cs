5ebe055324974cac71388a0ced9d5a59408293ed
gauravc574 <gauravchawre580@gmail.com>
2022-07-25 19:17:06 +0530

    Templated html helpers started

---
+++ MVCHtmlHempersDemo/Controllers/TemplatedHelpersDemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHtmlHempersDemo.Controllers
+{
+    public class TemplatedHelpersDemoController : Controller
+    {
+        // GET: TemplatedHelpersDemo
+        //Templated HTML helpers are used for :
+
+        //Display
+        
+
+
+        //Edit / Input
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
