2f6f2700864d266cb643578aefd9c18c798372fd
gauravc574 <gauravchawre580@gmail.com>
2022-07-27 23:33:22 +0530

    Jquery and Json done

---
+++ MVCJqueryDemo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using MVCJqueryDemo.Models;
+
+namespace MVCJqueryDemo.Controllers
+    
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetStudent() {
+
+            Student student = new Student() { 
+            Id =1,
+            Name="Gaurav"
+            };
+            var json = JsonConvert.SerializeObject(student);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCTempDataDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTempDataDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            TempData["myKey"] = "Data from index 1";
+            
+            return View();
+        }public ActionResult Index2()
+        {
+            TempData.Keep("myKey"); //keeps specific key for next http request cycle
+            //TempData.Keep(); //keeps all keys for next http request cycle
+            return View();
+        }public ActionResult Index3()
+        {
+            return View();
+        }public ActionResult Index4()
+        {
+            return View();
+        }public ActionResult Index5()
+        {
+            return View();
+        }
+    }
+}
