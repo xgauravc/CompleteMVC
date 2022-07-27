ccbf92dc5233f533d8e80c301a0a46e8806a484b
gauravc574 <gauravchawre580@gmail.com>
2022-07-27 13:06:59 +0530

    Custom Html Helpers done

---
+++ MVCHtmlHempersDemo/CustomHelperDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHtmlHempersDemo
+{
+    public static class CustomHelperDemo
+    {
+        //using static method
+        public static IHtmlString Image(string src, string alt) {
+
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>",src,alt));
+        }
+        //using Extension method
+        public static IHtmlString Img(this HtmlHelper htmlHelper, string src, string alt)
+        {
+
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'></img>", src, alt));
+        }
+        //using tagbuilder
+        public static IHtmlString ImgTag(this HtmlHelper htmlHelper, string src, string alt)
+        {
+            TagBuilder tagBuilder = new TagBuilder("img");
+            tagBuilder.Attributes.Add("src", src);
+            tagBuilder.Attributes.Add("alt", alt);
+
+            return new MvcHtmlString(tagBuilder.ToString());
+        }
+    }
+}
