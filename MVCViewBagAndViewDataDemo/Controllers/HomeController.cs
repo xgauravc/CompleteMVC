1dbd3c8c874a8e5b7fb1fc9e6b944c5a55d74a13
gauravc574 <gauravchawre580@gmail.com>
2022-07-27 13:51:57 +0530

    ViewBag and ViewData Done

---
+++ MVCViewBagAndViewDataDemo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MVCViewBagAndViewDataDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCViewBagAndViewDataDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>() 
+            {
+                new Employee{ EmpId =1, EmpName = "Gaurav", Department ="Engg"},
+                new Employee{ EmpId =2, EmpName = "Saurav", Department ="Engg"},
+                new Employee{ EmpId =3, EmpName = "Kaurav", Department ="Engg"}
+
+
+            };
+            ViewData["empList1"] = employees;
+
+            ViewBag.empList = employees;
+            return View();
+        }
+    }
+}
