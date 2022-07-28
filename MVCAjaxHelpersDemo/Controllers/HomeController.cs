56cf9f994420e2a901f841196f6b096c3243e93d
gauravc574 <gauravchawre580@gmail.com>
2022-07-28 12:34:44 +0530

    Ajasx BeginForm helpers done

---
+++ MVCAjaxHelpersDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MVCAjaxHelpersDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAjaxHelpersDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(Employee employee)
+        {
+            return Json(employee,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCAjaxHelpersDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAjaxHelpersDemo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+    }
+}
