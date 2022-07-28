57fc8ead4ea3dc7fafff050b3eea03e34d1b969d
gauravc574 <gauravchawre580@gmail.com>
2022-07-28 14:04:22 +0530

    Partial View Done

---
+++ MVCPartialViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPartialViewDemo.Models;
+
+namespace MVCPartialViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>() 
+            {
+            new Employee(){Name="Gaurav",Department="Engg",Designation= "SE",Role="Full Stack Developer" },
+            new Employee(){Name="Gaurav",Department="Engg",Designation= "SE",Role="Full Stack Developer" }
+            };
+            return View(employees);
+        }
+    }
+}
---
+++ MVCPartialViewDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPartialViewDemo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+        public string Designation { get; set; }
+    }
+}
