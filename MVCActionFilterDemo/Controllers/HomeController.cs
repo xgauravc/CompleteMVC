218a9867057ce3f282ae742fed9ce4c306eead3b
gauravc574 <gauravchawre580@gmail.com>
2022-07-29 22:35:57 +0530

    Output Cache done

---
+++ MVCActionFilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCActionFilterDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [OutputCache(Duration = 20,Location = System.Web.UI.OutputCacheLocation.Client)]
+        public ActionResult GetDate()
+        {
+            return View();
+        }
+    }
+}
